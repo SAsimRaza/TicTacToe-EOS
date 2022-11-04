9fd80974dae97cec9fd1bbcb09008a0e3aa598a1
SAsimRaza <asimraza2016@gmail.com>
2022-11-04 19:07:26 +0500

    feat: update close feature

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -9,24 +9,23 @@ CONTRACT tictactoe : public contract
 public:
     using contract::contract;
 
-    tictactoe(name reciever, name code, datastream<const char *> ds) : contract(reciever, code, ds), bettoken_symbol("ASA", 4)
+    tictactoe(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds), betting_token_symbol("ASA", 4)
     {
     }
 
     TABLE games
     {
-        name host;
+        // name host;
         name opponent;
         int64_t bet;
         bool status;
-        auto primary_key() const { return host.value; };
+        auto primary_key() const { return opponent.value; };
     };
 
     typedef multi_index<name("games"), games> games_table;
 
-    void create_game(name host, name opponent, int64_t bet_amount)
+    ACTION creategame(name host, name opponent, int64_t bet_amount)
     {
-
         games_table _games_host(get_self(), host.value);
         games_table _games_opponent(get_self(), opponent.value);
 
@@ -39,7 +38,7 @@ public:
             _games_host.emplace(
                 get_self(), [&](auto &newGame)
                 {
-            newGame.host = host;
+            // newGame.host = host;
             newGame.opponent = opponent;
             newGame.bet = bet_amount;
             newGame.status = false; });
@@ -57,17 +56,53 @@ public:
     [[eosio::on_notify("eosio.token::transfer ")]] ACTION create(name host, name _to, string opponentStr, asset amount)
     {
         auto opponent = name(opponentStr);
-        check(host != get_self(), "not allowed");
-        check(amount.symbol == bettoken_symbol && amount.amount > 0, "IV");
-        check(opponent != host && _to == get_self(), "Authorized User call this");
-        create_game(host, opponent, amount.amount);
+        // check(host != get_self(), "not allowed");
+        if (host != get_self())
+        {
+            print("Amount ", amount.amount);
+            print("Symbol ", amount.symbol);
+            // check(amount.symbol == betting_token_symbol, "IV");
+            // check(amount.amount > "0 ASA", "IA");
+            check(opponent != host && _to == get_self(), "Authorized User call this");
+            creategame(host, opponent, amount.amount);
+        }
     }
 
-    ACTION close()
+    ACTION close(name host, name opponent)
     {
-        // print("Hello,", host);
+        require_auth(host);
+
+        games_table _games(get_self(), host.value);
+        auto game_itr = _games.find(host.value);
+
+        check(game_itr != _games.end(), "Game doesnt exist");
+        check(game_itr->status == true, "game already accepted");
+
+        double payout_amount = game_itr->bet;
+        asset payout_asset(payout_amount, betting_token_symbol);
+
+        action payout_action = action(
+            permission_level{get_self(), "active"_n},
+            "eosio.token"_n,
+            "transfer"_n,
+            std::make_tuple(get_self(), host, payout_asset, string("Sent")));
+        payout_action.send();
+
+        _games.erase(game_itr);
+    }
+
+    ACTION deleteall(name scope)
+    {
+        require_auth(get_self());
+        games_table _games(get_self(), scope.value);
+
+        for (auto itr = _games.begin();
+             itr != _games.end();)
+        {
+            itr = _games.erase(itr);
+        }
     }
 
 private:
-    const symbol bettoken_symbol;
+    const symbol betting_token_symbol;
 };
