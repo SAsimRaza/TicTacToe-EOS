5ee91062e8e1e7750341621d8a356efb5f49b542
SAsimRaza <asimraza2016@gmail.com>
2022-11-05 20:50:17 +0500

    feat: remove comments

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -13,34 +13,74 @@ public:
     {
     }
 
-    TABLE games
+    static constexpr name none = "none"_n;
+    static constexpr name draw = "draw"_n;
+
+    // Declare game data structure.
+    struct [[eosio::table]] game
     {
-        // name host;
-        name opponent;
+
+        static constexpr uint16_t boardWidth = 3;
+        static constexpr uint16_t boardHeight = boardWidth;
+
+        game() : board(boardWidth * boardHeight, 0) {}
+
+        name challenger;
+        name host;
+        name turn;          // = account name of host/ challenger
+        name winner = none; // = none/ draw/ name of host/ name of challenger
         int64_t bet;
         bool status;
-        auto primary_key() const { return opponent.value; };
+        std::vector<uint8_t> board;
+
+        // Initialize board with empty cell
+        void initializeBoard()
+        {
+            board.assign(boardWidth * boardHeight, 0);
+        }
+
+        // Reset game
+        void resetGame()
+        {
+            initializeBoard();
+            turn = host;
+            winner = "none"_n;
+        }
+        auto primary_key() const { return challenger.value; }
+        EOSLIB_SERIALIZE(game, (challenger)(host)(turn)(winner)(board))
     };
+    // Define the games type which uses the game data structure.
+    typedef eosio::multi_index<"games"_n, game> games;
 
-    typedef multi_index<name("games"), games> games_table;
+    // TABLE games
+    // {
+    //     // name host;
+    //     name opponent;
+    //     int64_t bet;
+    //     bool status;
+    //     auto primary_key() const { return opponent.value; };
+    // };
 
-    ACTION creategame(name host, name opponent, int64_t bet_amount)
+    // typedef multi_index<name("games"), games> games_table;
+
+    ACTION creategame(name host, name challenger, int64_t bet_amount)
     {
-        games_table _games_host(get_self(), host.value);
-        games_table _games_opponent(get_self(), opponent.value);
+        games _games_host(get_self(), host.value);
+        games _games_opponent(get_self(), challenger.value);
 
         auto h_itr = _games_host.find(host.value);
         check(h_itr == _games_host.end(), "already exist");
 
-        auto o_itr = _games_opponent.find(opponent.value);
+        auto o_itr = _games_opponent.find(challenger.value);
         if (o_itr == _games_opponent.end())
         {
             _games_host.emplace(
                 get_self(), [&](auto &newGame)
                 {
-            newGame.opponent = opponent;
+            newGame.challenger = challenger;
             newGame.bet = bet_amount;
-            newGame.status = false; });
+            newGame.status = false;
+            newGame.turn = host; });
         }
         else
         {
@@ -63,11 +103,11 @@ public:
         }
     }
 
-    ACTION close(name host, name opponent)
+    ACTION close(name host, name challenger)
     {
         require_auth(host);
 
-        games_table _games(get_self(), host.value);
+        games _games(get_self(), host.value);
         auto game_itr = _games.find(host.value);
 
         check(game_itr != _games.end(), "Game doesnt exist");
@@ -86,10 +126,58 @@ public:
         _games.erase(game_itr);
     }
 
+    ACTION restart(const name &challenger, const name &host, const name &by)
+    {
+        check(has_auth(by), "Only " + by.to_string() + "can restart the game.");
+
+        // Check if game exists
+        games existingHostGames(get_self(), host.value);
+        auto itr = existingHostGames.find(challenger.value);
+        check(itr != existingHostGames.end(), "Game does not exist.");
+
+        // Check if this game belongs to the action sender
+        check(by == itr->host || by == itr->challenger, "This is not your game.");
+
+        // Reset game
+        existingHostGames.modify(itr, itr->host, [](auto &g)
+                                 { g.resetGame(); });
+    }
+
+    ACTION move(const name &challenger, const name &host, const name &by, const uint16_t &row, const uint16_t &column)
+    {
+        check(has_auth(by), "The next move should be made by " + by.to_string());
+
+        // Check if game exists
+        games existingHostGames(get_self(), host.value);
+        auto itr = existingHostGames.find(challenger.value);
+        check(itr != existingHostGames.end(), "Game does not exist.");
+
+        // Check if this game hasn't ended yet
+        check(itr->winner == none, "The game has ended.");
+
+        // Check if this game belongs to the action sender
+        check(by == itr->host || by == itr->challenger, "This is not your game.");
+        // Check if this is the  action sender's turn
+        check(by == itr->turn, "it's not your turn yet!");
+
+        // Check if user makes a valid movement
+        check(isValidMove(row, column, itr->board), "Not a valid movement.");
+
+        // Fill the cell, 1 for host, 2 for challenger
+        // TODO could use constant for 1 and 2 as well
+        const uint8_t cellValue = itr->turn == itr->host ? 1 : 2;
+        const auto turn = itr->turn == itr->host ? itr->challenger : itr->host;
+        existingHostGames.modify(itr, itr->host, [&](auto &g)
+                                 {
+        g.board[row * game::boardWidth + column] = cellValue;
+        g.turn = turn;
+        g.winner = getWinner(g); });
+    }
+
     ACTION deleteall(name scope)
     {
         require_auth(get_self());
-        games_table _games(get_self(), scope.value);
+        games _games(get_self(), scope.value);
 
         for (auto itr = _games.begin();
              itr != _games.end();)
@@ -100,4 +188,70 @@ public:
 
 private:
     const symbol betting_token_symbol;
+
+    bool isEmptyCell(const uint8_t &cell)
+    {
+        return cell == 0;
+    }
+
+    bool isValidMove(const uint16_t &row, const uint16_t &column, const std::vector<uint8_t> &board)
+    {
+        uint32_t movementLocation = row * game::boardWidth + column;
+        bool isValid = movementLocation < board.size() && isEmptyCell(board[movementLocation]);
+        return isValid;
+    }
+
+    name getWinner(const game &currentGame)
+    {
+        auto &board = currentGame.board;
+
+        bool isBoardFull = true;
+
+        // Use bitwise AND operator to determine the consecutive values of each column, row and diagonal
+        // Since 3 == 0b11, 2 == 0b10, 1 = 0b01, 0 = 0b00
+        std::vector<uint32_t> consecutiveColumn(game::boardWidth, 3);
+        std::vector<uint32_t> consecutiveRow(game::boardHeight, 3);
+        uint32_t consecutiveDiagonalBackslash = 3;
+        uint32_t consecutiveDiagonalSlash = 3;
+
+        for (uint32_t i = 0; i < board.size(); i++)
+        {
+            isBoardFull &= isEmptyCell(board[i]);
+            uint16_t row = uint16_t(i / game::boardWidth);
+            uint16_t column = uint16_t(i % game::boardWidth);
+
+            // Calculate consecutive row and column value
+            consecutiveRow[column] = consecutiveRow[column] & board[i];
+            consecutiveColumn[row] = consecutiveColumn[row] & board[i];
+            // Calculate consecutive diagonal \ value
+            if (row == column)
+            {
+                consecutiveDiagonalBackslash = consecutiveDiagonalBackslash & board[i];
+            }
+            // Calculate consecutive diagonal / value
+            if (row + column == game::boardWidth - 1)
+            {
+                consecutiveDiagonalSlash = consecutiveDiagonalSlash & board[i];
+            }
+        }
+
+        // Inspect the value of all consecutive row, column, and diagonal and determine winner
+        std::vector<uint32_t> aggregate = {consecutiveDiagonalBackslash, consecutiveDiagonalSlash};
+        aggregate.insert(aggregate.end(), consecutiveColumn.begin(), consecutiveColumn.end());
+        aggregate.insert(aggregate.end(), consecutiveRow.begin(), consecutiveRow.end());
+
+        for (auto value : aggregate)
+        {
+            if (value == 1)
+            {
+                return currentGame.host;
+            }
+            else if (value == 2)
+            {
+                return currentGame.challenger;
+            }
+        }
+        // Draw if the board is full, otherwise the winner is not determined yet
+        return isBoardFull ? draw : none;
+    }
 };
